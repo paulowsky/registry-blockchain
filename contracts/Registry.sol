3fbb81d06dbbc81f466f8c04d48668fb96daf45f
Paulo Cesar <paulocitron99@gmail.com>
2022-05-20 01:43:29 -0300

    feat: add hashes for signatories

--- contracts/Registry.sol
+++ contracts/Registry.sol
@@ -6,9 +6,9 @@ contract Registry {
   uint public contractsCount = 0;
 
   struct Signatory {
-    address id;
+    bytes32 id;
+    address signatoryAddress;
     string name;
-    bool signed;
     uint datetimeSigned;
   }
 
@@ -16,9 +16,10 @@ contract Registry {
     uint id;
     string content;
     uint datetimeCreated;
-    mapping(address => Signatory) signatories;
-    address[] signatoriesAddress;
-    bool allSigned;
+    mapping(bytes32 => Signatory) signatories;
+    bytes32[] signHashes;
+    bytes32 creatorSignatoryId;
+    uint amountPendingSignatures;
   }
 
   mapping(uint => Contract) public contracts;
@@ -38,61 +39,90 @@ contract Registry {
   function createContract(
     string memory _name,
     string memory _content,
-    Signatory[] memory _signatories
+    uint256 _amountSignatories
   ) public {
     uint _datetimeCreated = block.timestamp;
+    bytes32 _creatorSignatoryId = keccak256(
+      abi.encodePacked(
+        msg.sender,
+        _datetimeCreated,
+        _name
+      )
+    );
 
     Contract storage newContract = contracts[contractsCount];
     newContract.content = _content;
     newContract.datetimeCreated = _datetimeCreated;
-    newContract.signatoriesAddress = new address[](_signatories.length + 1);
+    newContract.creatorSignatoryId = _creatorSignatoryId;
+    newContract.amountPendingSignatures = _amountSignatories;
+    newContract.signHashes = new bytes32[](_amountSignatories + 1);
 
-    newContract.signatories[msg.sender] = Signatory({
-      id: msg.sender,
+    newContract.signatories[_creatorSignatoryId] = Signatory({
+      id: _creatorSignatoryId,
+      signatoryAddress: msg.sender,
       name: _name,
-      signed: true,
       datetimeSigned: _datetimeCreated
     });
+    newContract.signHashes.push(_creatorSignatoryId);
 
-    newContract.signatoriesAddress.push(msg.sender);
-
-    if (_signatories.length == 0) {
-      newContract.allSigned = true;
+    for(uint64 i = 0; i < _amountSignatories; i++) {
+      newContract.signHashes.push(
+        keccak256(
+          abi.encodePacked(
+            msg.sender,
+            _datetimeCreated,
+            i
+          )
+        )
+      );
     }
 
-    for(uint i = 0; i < _signatories.length; i++) {
-      newContract.signatories[_signatories[i].id] = _signatories[i];
-      newContract.signatoriesAddress.push(_signatories[i].id);
-    }
-
-    contractsCount ++;
+    contractsCount++;
 
     emit ContractCreated(contractsCount, _datetimeCreated);
   }
 
-  function signContract(uint _contractId) public {
-    require(_contractId < contractsCount);
-    require(contracts[_contractId].signatories[msg.sender].id == msg.sender);
-    require(contracts[_contractId].signatories[msg.sender].signed == false);
+  function signContract(
+    uint _contractId,
+    bytes32 _signHash,
+    string memory _name
+  ) public {
+    require(_contractId >= 0 && _contractId < contractsCount); // check if id is valid
 
-    uint _datetimeSigned = block.timestamp;
-    bool _allSigned = true;
+    Contract storage _contract = contracts[_contractId];
 
-    address[] memory _signatoriesAddress = contracts[_contractId].signatoriesAddress;
-    Signatory memory _signatory = contracts[_contractId].signatories[msg.sender];
+    require(contracts[_contractId].amountPendingSignatures > 0); // check if the contract need more signatures
 
-    _signatory.signed = true;
-    _signatory.datetimeSigned = _datetimeSigned;
+    require(_contract.signatories[_signHash].datetimeSigned == 0); // check if hash is not used
 
-    for(uint i = 0; i < _signatoriesAddress.length; i++){
-      if (contracts[_contractId].signatories[_signatoriesAddress[i]].signed == false) {
-        _allSigned = false;
+    bool _isValidHash = false;
+    for (uint i=0; i < _contract.signHashes.length; i++) {
+      if (_signHash == _contract.signHashes[i]) {
+        _isValidHash = true;
         break;
       }
     }
+    require(_isValidHash);
 
-    contracts[_contractId].signatories[msg.sender] = _signatory;
-    contracts[_contractId].allSigned = _allSigned;
+    bool _senderNotSigned = true;
+    for (uint i=0; i < _contract.signHashes.length; i++) {
+      if (_contract.signatories[_contract.signHashes[i]].signatoryAddress == msg.sender) {
+        _senderNotSigned = false;
+        break;
+      }
+    }
+    require(_senderNotSigned);
+
+    uint _datetimeSigned = block.timestamp;
+
+    Signatory memory _signatory = contracts[_contractId].signatories[_signHash];
+
+    _signatory.signatoryAddress = msg.sender;
+    _signatory.name = _name;
+    _signatory.datetimeSigned = _datetimeSigned;
+
+    contracts[_contractId].signatories[_signHash] = _signatory;
+    contracts[_contractId].amountPendingSignatures--;
 
     emit ContractSigned(_contractId, _datetimeSigned);
   }
